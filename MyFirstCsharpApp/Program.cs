7f360a6fc3839d90c33b1d2cf372f953e2efff59
jhc <jhc@gmail.com>
2025-12-29 09:30:33 +0100

    variables and functions

--- MyFirstCsharpApp/Program.cs
+++ MyFirstCsharpApp/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+static string HandleNullInput(string? input) 
+{
+    if (input == null || input.Trim() == "") 
+    {
+        return "Guest";
+    }
+    return input;
+}
+
+static string Greeting(string name) 
+{
+    return $"Hello, {name}!";
+}
+
+Console.WriteLine("What is your name?");
+string? name = Console.ReadLine();
+name = HandleNullInput(name);
+Console.WriteLine(Greeting(name));
