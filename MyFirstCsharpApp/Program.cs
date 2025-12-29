2b00995a468627fe87094337ca3c9cb92574a890
jhc <jhc@gmail.com>
2025-12-29 09:41:29 +0100

    simple classes

--- MyFirstCsharpApp/Program.cs
+++ MyFirstCsharpApp/Program.cs
@@ -1,20 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-static string HandleNullInput(string? input) 
+string name = Greeter.GetName();
+Greeter.Greet(name);
+class Greeter
 {
-    if (input == null || input.Trim() == "") 
+    public static string GetName() 
     {
-        return "Guest";
+        Console.WriteLine("What is your name?");
+        string? input = Console.ReadLine();
+        return HandleNullInput(input);
     }
-    return input;
-}
+    public static string HandleNullInput(string? input) 
+    {
+        if (input == null || input.Trim() == "") 
+        {
+            return "Guest";
+        }
+        return input;
+    }
 
-static string Greeting(string name) 
-{
-    return $"Hello, {name}!";
+    public static void Greet(string name) 
+    {
+        Console.WriteLine($"Hello, {name}!");
+    }
 }
-
-Console.WriteLine("What is your name?");
-string? name = Console.ReadLine();
-name = HandleNullInput(name);
-Console.WriteLine(Greeting(name));
